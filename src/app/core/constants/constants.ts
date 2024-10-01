c27c287c6172c0bab4d2aec558507cf8c7697817
omar karake <omar.karake@amalitechtraining.org>
2024-10-01 12:12:06 +0200

    connecting with firebase, and setting up some functionality like add, delete, update and get

---
+++ src/app/core/constants/constants.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB909F-YqkvsXl_EXd34SKPyIDB6X9Z8y8',
+  authDomain: 'fir-crud-app-cac59.firebaseapp.com',
+  projectId: 'fir-crud-app-cac59',
+  storageBucket: 'fir-crud-app-cac59.appspot.com',
+  messagingSenderId: '225561738484',
+  appId: '1:225561738484:web:cd60719c9e1a25381332a1',
+  measurementId: 'G-4PTWGBT0DX',
+};
--- src/app/core/services/expense.service.ts
+++ src/app/core/services/expense.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+} from '@angular/fire/compat/database';
+import { IExpense } from '../models/common.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExpenseService {
+  private dbPath = '/expenses';
+  expensesRef: AngularFireList<any>;
+  constructor(private db: AngularFireDatabase) {
+    this.expensesRef = db.list(this.dbPath);
+  }
 
-  constructor() { }
+  getAllExpenses() {
+    return this.expensesRef;
+  }
+
+  addExpense(expense: IExpense) {
+    return this.expensesRef.push(expense);
+  }
+
+  getExpenseById(key: string) {
+    return this.db.object(`${this.dbPath}/${key}`);
+  }
+
+  deleteExpense(key: string) {
+    return this.expensesRef.remove(key);
+  }
+
+  updateExpense(key: string, expense: IExpense) {
+    return this.expensesRef.update(key, expense);
+  }
 }
